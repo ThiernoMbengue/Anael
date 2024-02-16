8dbc31efa5d308d583e5f9a0ead7b81c6c52917f
Anaël Delorme <87323681+anaelDelorme@users.noreply.github.com>
2024-02-16 12:06:37 +0000

    init with video background

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,45 +1,87 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var scroller = scrollama();
-    var title = document.querySelector('h1');
-    var titleHeight = title.offsetHeight;
-    var lastScrollTop = 0;
-    var images = [
-        "./images/carte.svg",
-        "https://via.placeholder.com/600x400/00FFFF/808080?text=2",
-        "./images/pieHuile.svg",
-        "https://via.placeholder.com/600x400/FF0000/808080?text=4",
-        "https://via.placeholder.com/600x400/00FFFF/808080?text=5",
-        "https://via.placeholder.com/600x400/00FFFF/808080?text=3"
-    ];
-    function handleStepEnter(response) {
-        var stepElement = response.element;
-        var container = stepElement.closest('.content, .additional-content');
-        var img = container.querySelector('.image-container img');
-        if (img) {
-            img.src = images[response.index];
-        }
-    }
-
-    function init() {
-        scroller.setup({
-            step: '.step',
-            offset: 0.5,
-            debug: false
-        }).onStepEnter(handleStepEnter);
-
-        window.addEventListener('scroll', function() {
-            var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-            if (currentScroll > lastScrollTop && currentScroll > titleHeight) {
-                // Faites défiler vers le bas
-                title.classList.add('title-hide');
-            } else {
-                // Faites défiler vers le haut
-                title.classList.remove('title-hide');
-            }
-            lastScrollTop = currentScroll;
-        });
-    }
-
-    init();
+          // using d3 for convenience
+		  var main = d3.select("main");
+		  var scrolly = main.select("#scrolly");
+		  var figure = scrolly.select("figure");
+		  var article = scrolly.select("article");
+		  var step = article.selectAll(".step");
+		  const caption = document.getElementById("captionObj");
+	
+		  // initialize the scrollama
+		  var scroller = scrollama();
+	
+		  // generic window resize listener event
+		  function handleResize() {
+			// 1. update height of step elements
+			var stepH = Math.floor(window.innerHeight * 0.75);
+			step.style("height", stepH + "px");
+	
+			var figureHeight = window.innerHeight / 2;
+			var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+	
+			figure
+			  .style("height", figureHeight + "px")
+			  .style("top", figureMarginTop + "px");
+	
+			// 3. tell scrollama to update new element dimensions
+			scroller.resize();
+		  }
+	
+		  // scrollama event handlers
+		  function handleStepEnter(response) {
+			console.log(response);
+			// response = { element, direction, index }
+	
+			// add color to current step only
+			step.classed("is-active", function (d, i) {
+			  return i === response.index;
+			});
+	
+			// update graphic based on step
+			figure.select("p").text(response.index + 1);
+	
+			document
+			  .getElementById(response.element.dataset.target)
+			  .setAttribute(
+				response.element.dataset.attribute,
+				response.element.dataset.value
+			  );
+			if (response.element.dataset.target == "imgObj") {
+			  let alt = response.element.dataset.text;
+			  if (alt) {
+				caption.innerText = alt;
+			  }
+			}
+		  }
+	
+		  function setupStickyfill() {
+			d3.selectAll(".sticky").each(function () {
+			  Stickyfill.add(this);
+			});
+		  }
+	
+		  function init() {
+			setupStickyfill();
+	
+			// 1. force a resize on load to ensure proper dimensions are sent to scrollama
+			handleResize();
+	
+			// 2. setup the scroller passing options
+			// 		this will also initialize trigger observations
+			// 3. bind scrollama event handlers (this can be chained like below)
+			scroller
+			  .setup({
+				step: "#scrolly article .step",
+				offset: 0.33,
+				debug: false,
+			  })
+			  .onStepEnter(handleStepEnter);
+	
+			// setup resize event
+			window.addEventListener("resize", handleResize);
+		  }
+	
+		  // kick things off
+		  init();
 });
